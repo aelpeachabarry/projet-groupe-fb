2571ea1d165405fb666324acc7200722b4e6f6e0
Barry Aelpeacha <aelpeacha.barry@gmail.com>
2015-04-21 09:21:09 +0200

    ajout des nouveaux identifiants + liens vers heroku

--- app/Facebook/constants.php
+++ app/Facebook/constants.php
@@ -5,6 +5,6 @@
  * Date: 21/04/15
  * Time: 08:16
  */
-define('APP_ID','886986474691886');
-define('APP_SECRET','31b9e4fad83bb45b89b75993c6aaf981');
-define('REDIRECT_URL','http://localhost:8888/');
+define('APP_ID','384491318402733');
+define('APP_SECRET','dab4c606e28695176a9d99dcc2a813c8');
+define('REDIRECT_URL','https://projet-groupe-fb.herokuapp.com/');
