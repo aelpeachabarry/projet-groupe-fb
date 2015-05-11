072bafac5fb9a0787c82d128d57e4424993e4727
mrakotomizao <mrakotomizao@gmail.com>
2015-05-12 00:16:44 +0200

    Add to db info

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ if(is_string($user)){
     echo '<a href="'.$user.'">Se connecter avec facebook</a>';
 
 }else{
-    var_dump($user);
+    var_dump($user->getProperty('id'));
     ?>
     <form method="post" action="<?php $_SERVER['PHP_SELF'] ?>" enctype="multipart/form-data">
         <input type="file" name="mon_fichier" id="mon_fichier" /><br />
