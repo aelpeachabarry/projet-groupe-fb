5569a4dfc253f00fea153d4bcf4568272b572e1c
mrakotomizao <mrakotomizao@gmail.com>
2015-06-17 01:23:19 +0200

    ajout des css et js

--- index.php
+++ index.php
@@ -73,6 +73,7 @@ if(is_string($user)){
 
             foreach($images->getImages() as $image){
                 echo $image->id;
+                echo "<img src='https://graph.facebook.com/".$connect->getFacebookId()."/".$image->id."'>";
             }
         }
     }
