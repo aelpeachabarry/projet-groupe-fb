587ad7611d5009c6a1a309a85a56dc280cf052ee
mrakotomizao <mrakotomizao@gmail.com>
2015-06-17 07:30:56 +0200

    ajout des css et js

---
+++ ajaxHandler/functions.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guizmo
+ * Date: 17/06/2015
+ * Time: 00:48
+ */
+namespace ajaxHandler;
+use App\Facebook\ImageManager;
+
+
+if(isset($_POST['action'])){
+    if(function_exists($_POST['action'])){
+        eval($_POST['action']."()");
+    }else{
+        echo 'function doesn\'t exist';
+    }
+}
+function getImages(){
+    echo "yes";
+}
--- assets/js/travelinfo.js
+++ assets/js/travelinfo.js
@@ -1,3 +1,16 @@
 /**
  * Created by guizmo on 17/06/2015.
  */
+$(document).ready(function(){
+    $("#selectbasic").change(function(){
+        $.ajax({
+            url:"/ajaxHandler/functions.php",
+            data : {
+                action : 'getImages'
+            },
+            success:function(data) {
+                return data;
+            }
+        });
+    });
+});
