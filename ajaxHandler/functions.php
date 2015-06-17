cdae3decac7e7baa1427f504fb11b579539952c7
mrakotomizao <mrakotomizao@gmail.com>
2015-06-17 08:05:35 +0200

    ajout des css et js

--- ajaxHandler/functions.php
+++ ajaxHandler/functions.php
@@ -5,18 +5,10 @@
  * Date: 17/06/2015
  * Time: 00:48
  */
-namespace ajaxHandler;
-use App\Facebook\ImageManager;
-
 
 if(isset($_POST['action'])){
     var_dump( $_POST['action']);
-    if(function_exists('getImages')){
-        $_POST['action']();
-        var_dump($_POST['action']());
-    }else{
-        echo 'function doesn\'t exist';
-    }
+    getImages();
 }
 function getImages(){
     return "yes";
