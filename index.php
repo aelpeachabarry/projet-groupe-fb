0dfeda5c79786660e60a8bcdcd1e638873f458cf
mrakotomizao <mrakotomizao@gmail.com>
2015-06-18 23:16:57 +0200

    ajout des css et js

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ require 'app/Facebook/constants.php';
     <meta charset="utf-8" />
     <title>Concours TravelInfo</title>
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
-    <link href="assets/css/image-picker.css.css" rel="stylesheet" />
+    <link href="assets/css/image-picker.css" rel="stylesheet" />
     <script type="text/javascript" src="assets/js/jquery-2.1.4.min.js"></script>
     <script type="text/javascript" src="assets/js/bootstrap.js"></script>
     <script type="text/javascript" src="assets/js/travelinfo.js"></script>
