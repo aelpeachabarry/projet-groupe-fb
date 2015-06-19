3b9b0388e49c77692dc91397d75d428568003aa5
Newwer21 <j-jac_ly@hotmail.fr>
2015-06-19 15:01:16 +0200

    Ajout d'une connexion à Heroku

--- constants.php
+++ constants.php
@@ -7,6 +7,16 @@
  */
 
 define('ROOT', dirname(__FILE__));
-define('USER_DB', 'newwer21');
-define('PWD_DB', '');
-define('DB_NAME', 'concours-fb');
+
+/* Local */
+
+//define('HOST', 'localhost');
+//define('USER_DB', 'newwer21');
+//define('PWD_DB', '');
+//define('DB_NAME', 'concours-fb');
+
+/* Heroku */
+define('HOST', 'ec2-54-217-202-110.eu-west-1.compute.amazonaws.com');
+define('USER_DB', 'nyhdkvyrzcmgtp');
+define('PWD_DB', '2Dup2eYAc5NsATuKU7y0moHVaJ');
+define('DB_NAME', 'dmqsb6naos1ca');
