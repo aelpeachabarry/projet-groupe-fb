8b4d198c9a8ce15b0241693d42ec554e8a4409e0
mrakotomizao <mrakotomizao@gmail.com>
2015-06-20 18:49:17 +0200

    Merging alpha and Moise work

---
+++ assets/js/travelagency.js
@@ -0,0 +1,25 @@
+$('#participer').click(function(e){
+    e.preventDefault();
+    console.log('here');
+    $.ajax({
+        url : '/projet-groupe-fb/functions/connectFb.php', // La ressource ciblée
+        type:'get',
+        success : function(url){
+            document.location.href=url;
+        }
+    });
+});
+$("#selectbasic").change(function(){
+    if(this.val()!="" || undefined){
+        $.ajax({
+            url:"/functions/getImages.php",
+            type:'POST',
+            data : {
+                findImg : this.val()
+            },
+            success:function(data) {
+                return data;
+            }
+        });
+    }
+});
