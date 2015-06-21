f790d1e31b1848668912ee1f7ae5307ad9f8207c
mrakotomizao <mrakotomizao@gmail.com>
2015-06-21 21:17:11 +0200

    delete .lock et .json

--- views/landing.php
+++ views/landing.php
@@ -9,8 +9,11 @@
                     <h2 class="text-center">L'holidays selfies est ouvert</h2>
                     <p class="lead text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos debitis provident nulla illum minus enim praesentium repellendus ullam cupiditate reiciendis optio voluptatem, recusandae nobis quis aperiam, sapiente libero ut at.</p>
                     <?php
-                        var_dump($user);
+                        if(is_object($user)){
                         echo '<a href="#" id="participer" class="btn btn-primary btn-primary" style="margin-left:25%">Participer</a>';
+                        }else{
+                            echo "<h4>Bienvenu ".$user->getName()."</h4>";
+                        }
                     ?>
                 </div>
             </div>
