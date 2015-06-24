36a6a90290268e740cb90ee880f1e606fb210727
mrakotomizao <mrakotomizao@gmail.com>
2015-06-25 00:27:04 +0200

    ajout de la partie Upload

--- assets/js/travelagency.js
+++ assets/js/travelagency.js
@@ -1,26 +1,28 @@
-$('#participer').click(function(e){
-    e.preventDefault();
-    console.log('here');
-    $.ajax({
-        url : '/functions/connectFb.php', // La ressource ciblée
-        type:'get',
-        success : function(url){
-            document.location.href=url;
-        }
-    });
-});
-$("#selectbasic").change(function(){
-    if(this.val()!="" || undefined){
+$(document).ready(function(){
+    $('#participer').click(function(e){
+        e.preventDefault();
+        console.log('here');
         $.ajax({
-            url:"/functions/getImages.php",
-            type:'POST',
-            data : {
-                findImg : this.val()
-            },
-            success:function(data) {
-                return data;
+            url : '/functions/connectFb.php', // La ressource ciblée
+            type:'get',
+            success : function(url){
+                document.location.href=url;
             }
         });
-    }
+    });
+    $("#selectbasic").change(function(){
+        if(this.val()!="" || undefined){
+            $.ajax({
+                url:"/functions/getImages.php",
+                type:'POST',
+                data : {
+                    findImg : this.val()
+                },
+                success:function(data) {
+                    return data;
+                }
+            });
+        }
+    });
+    $(".image-picker").imagepicker();
 });
-$(".image-picker").imagepicker();
