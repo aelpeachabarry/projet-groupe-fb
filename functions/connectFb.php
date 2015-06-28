97d317a5fcdbf450b442e3e1c4d6a1ff48e6ba36
mrakotomizao <mrakotomizao@gmail.com>
2015-06-28 12:20:17 +0200

    Correction de quelque erreur dans Upload

--- functions/connectFb.php
+++ functions/connectFb.php
@@ -7,9 +7,22 @@ session_start();
 use \App\Facebook\FacebookConnect;
 require '../vendor/autoload.php';
 require '../app/Facebook/constants.php';
+require 'model/UserModel.php';
 
 $connect = new FacebookConnect(APP_ID, APP_SECRET);
 $user = $connect->connect(REDIRECT_URL);
+
+$userManager = new UserModel();
+$nonEscape = [
+    'is_participant' => 1
+];
+$escape = [
+    'id_facebook' => $user->getId(),
+    'nom' => $user->getLastName(),
+    'prenom' => $user->getFirstName(),
+    'email' => $user->getEmail()
+];
+$userManager->create($nonEscape,$escape);
 
 if(is_string($user)){
     echo $user;
