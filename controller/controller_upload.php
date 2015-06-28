04e0ebbc7f066ee8c59b616c7d78b6db0fb00bb1
mrakotomizao <mrakotomizao@gmail.com>
2015-06-28 11:29:29 +0200

    Correction de quelque erreur dans Upload

--- controller/controller_upload.php
+++ controller/controller_upload.php
@@ -12,11 +12,12 @@ class ControllerUpload{
             'id_photo' => $idPhoto,
             'id_facebook' => $idUser,
             'id_concours' => 4,
-            'last_update' => date('Y-m-d G:i:s'),
+
 
         ];
         $escape = [
-            'details' => $detail
+            'details' => $detail,
+            'last_update' => date('Y-m-d G:i:s'),
         ];
         $imgManager->create($nonescape,$escape);
 
