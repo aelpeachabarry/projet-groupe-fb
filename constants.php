2a76e7ea2f57858f6a4f777876fd282fd4a32880
mrakotomizao <mrakotomizao@gmail.com>
2015-06-28 12:58:34 +0200

    Correction de quelque erreur dans Upload

--- constants.php
+++ constants.php
@@ -19,4 +19,3 @@ define('HOST', 'ec2-54-225-154-5.compute-1.amazonaws.com');
 define('USER_DB', 'xbhzyyoziowdlx');
 define('PWD_DB', 'ICVHJx9jSI-oRmpxvsT6VM3c-l');
 define('DB_NAME', 'dagidi42jnneae');
-define('ROOTPATH', __DIR__);
