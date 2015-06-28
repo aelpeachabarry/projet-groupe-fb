cc7fde1dce349e2a576cb7e73818ff00e436da14
mrakotomizao <mrakotomizao@gmail.com>
2015-06-28 13:07:32 +0200

    Correction de quelque erreur dans Upload

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -12,4 +12,29 @@ $(document).ready(function(){
     //caroussel landing page
     $(".banner-image").backstretch(imgArray, {duration: 3000, fade: 750});
 
+    $('#participer').click(function(e){
+        e.preventDefault();
+        console.log('here');
+        $.ajax({
+            url : '/functions/connectFb.php', // La ressource ciblée
+            type:'get',
+            success : function(url){
+                document.location.href=url;
+            }
+        });
+    });
+    $("#selectbasic").change(function(){
+        if(this.val()!="" || undefined){
+            $.ajax({
+                url:"/functions/getImages.php",
+                type:'POST',
+                data : {
+                    findImg : this.val()
+                },
+                success:function(data) {
+                    return data;
+                }
+            });
+        }
+    });
 });
