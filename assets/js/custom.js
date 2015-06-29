6b2f4c0c7ece1b6ddfb99aee5d3776819b58355e
mrakotomizao <mrakotomizao@gmail.com>
2015-06-30 01:10:40 +0200

    Modification du script d'ajout d'image dynamique

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -14,6 +14,7 @@ $('document').ready(function() {
                 }
             });
         }
+        $(".image-picker").imagepicker({show_label: true});
     });
-    $(".image-picker").imagepicker({show_label: true});
+
 });
