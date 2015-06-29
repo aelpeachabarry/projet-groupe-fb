60ec3b9bae70960314afb271373dab2f711256c4
mrakotomizao <mrakotomizao@gmail.com>
2015-06-29 16:53:27 +0200

    Modification de l'upload

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,7 @@
 $('document').ready(function() {
     $("#selectbasic").change(function(){
         console.log($(this));
-        if($(this).val()!="" || $(this).val()!=undefined){
+        if($(this).val()!=null){
             $.ajax({
                 url:"/functions/getImages.php",
                 type:'POST',
