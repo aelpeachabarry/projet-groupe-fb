11cf11381bf3e4b755ee772e409807c3d56f91d8
mrakotomizao <mrakotomizao@gmail.com>
2015-06-29 18:31:12 +0200

    Modification de l'upload

--- functions/getImages.php
+++ functions/getImages.php
@@ -5,22 +5,21 @@
  * Date: 18/06/2015
  * Time: 23:57
  */
-var_dump($_POST);
-//if(isset($_POST['findImg'])){
-//    $output = "";
-//    if($_POST['selectbasic']=="default"){
-//        echo "<p>Veuillez Selectionnez un album</p>";
-//    }else{
-//        $images = new ImageManager($connect->getSession(),$_POST['selectbasic']);
-//        $output = '<select class="image-picker show-labels show-html">';
-//        foreach($images->getImages() as $image){
-//            /*var_dump($image);*/
-//            $output .= "<option data-img-src='".$image->source."' value='".$image->id."'>".$image->name."</option>";
-//            /*echo "<img src='".$image->source."'>";*/
-//        }
-//        $output .= '</select>';
-//        echo $output;
-//    }
-//}else{
-//    echo 'probleme';
-//}
+if(isset($_POST["findImg"])){
+    $output = "";
+    if($_POST["findImg"]=="default"){
+        echo "<p>Veuillez Selectionnez un album</p>";
+    }else{
+        $images = new ImageManager($connect->getSession(),$_POST['selectbasic']);
+        $output = '<select class="image-picker show-labels show-html">';
+        foreach($images->getImages() as $image){
+            /*var_dump($image);*/
+            $output .= "<option data-img-src='".$image->source."' value='".$image->id."'>".$image->name."</option>";
+            /*echo "<img src='".$image->source."'>";*/
+        }
+        $output .= '</select>';
+        echo $output;
+    }
+}else{
+    echo 'probleme';
+}
