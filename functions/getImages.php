542c818b7c98c963f5a2e33ab2a5e7a435d69d51
mrakotomizao <mrakotomizao@gmail.com>
2015-06-30 00:17:48 +0200

    Modification du script d'ajout d'image dynamique

--- functions/getImages.php
+++ functions/getImages.php
@@ -9,29 +9,29 @@ session_start();
 require $_SERVER['DOCUMENT_ROOT'].'/app/Facebook/constants.php';
 use \App\Facebook\FacebookConnect;
 require $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
-var_dump($_SESSION);
+
 $connect = new FacebookConnect(APP_ID, APP_SECRET);
 $user = $connect->connect(REDIRECT_URL);
 
 
-//
-//if($_POST["findImg"]){
-//    $output = "";
-//    if($_POST["findImg"]=="default"){
-//        echo "<p>Veuillez Selectionnez un album</p>";
-//    }else{
-//        echo $_SESSION;
-//        $images = new ImageManager($connect->getSession(),$_POST['selectbasic']);
-//        $output = '<select class="image-picker show-labels show-html">';
-//        foreach($images->getImages() as $image){
-//            /*var_dump($image);*/
-//            $output .= "<option data-img-src='".$image->source."' value='".$image->id."'>".$image->name."</option>";
-//            /*echo "<img src='".$image->source."'>";*/
-//        }
-//        $output .= '</select>';
-//        echo $output;
-//    }
-//    echo "probleme";
-//}else{
-//    echo 'probleme';
-//}
+
+if($_POST["findImg"]){
+    $output = "";
+    if($_POST["findImg"]=="default"){
+        echo "<p>Veuillez Selectionnez un album</p>";
+    }else{
+        echo $_SESSION;
+        $images = new ImageManager($connect->getSession(),$_POST['selectbasic']);
+        $output = '<select class="image-picker show-labels show-html">';
+        foreach($images->getImages() as $image){
+            /*var_dump($image);*/
+            $output .= "<option data-img-src='".$image->source."' value='".$image->id."'>".$image->name."</option>";
+            /*echo "<img src='".$image->source."'>";*/
+        }
+        $output .= '</select>';
+        echo $output;
+    }
+    echo "probleme";
+}else{
+    echo 'probleme';
+}
