de1bac12a73c34768e56a155e2d723ac39d84686
mrakotomizao <mrakotomizao@gmail.com>
2015-06-29 16:37:19 +0200

    Modification de l'upload

--- model/ConcoursModel.php
+++ model/ConcoursModel.php
@@ -5,8 +5,7 @@
  * Date: 27/06/15
  * Time: 22:59
  */
-require '../constants.php';
-require ROOT.'core/Model.php';
+require $_SERVER['DOCUMENT_ROOT'].'core/Model.php';
 
 class ConcoursModel extends abstractModel{
     public function __construct(){
