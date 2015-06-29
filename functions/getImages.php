03c06cacded97f19925d0c58bb3a5ef3103a5c57
mrakotomizao <mrakotomizao@gmail.com>
2015-06-29 18:43:52 +0200

    Modification du script d'ajout d'image dynamique

--- functions/getImages.php
+++ functions/getImages.php
@@ -5,22 +5,23 @@
  * Date: 18/06/2015
  * Time: 23:57
  */
-if(isset($_POST["findImg"])){
-    $output = "";
-    if($_POST["findImg"]=="default"){
-        echo "<p>Veuillez Selectionnez un album</p>";
-    }else{
-        $images = new ImageManager($connect->getSession(),$_POST['selectbasic']);
-        $output = '<select class="image-picker show-labels show-html">';
-        foreach($images->getImages() as $image){
-            /*var_dump($image);*/
-            $output .= "<option data-img-src='".$image->source."' value='".$image->id."'>".$image->name."</option>";
-            /*echo "<img src='".$image->source."'>";*/
-        }
-        $output .= '</select>';
-        echo $output;
-    }
-    echo "probleme";
-}else{
-    echo 'probleme';
-}
+echo $_POST["findImg"];
+//if($_POST["findImg"]){
+//    $output = "";
+//    if($_POST["findImg"]=="default"){
+//        echo "<p>Veuillez Selectionnez un album</p>";
+//    }else{
+//        $images = new ImageManager($connect->getSession(),$_POST['selectbasic']);
+//        $output = '<select class="image-picker show-labels show-html">';
+//        foreach($images->getImages() as $image){
+//            /*var_dump($image);*/
+//            $output .= "<option data-img-src='".$image->source."' value='".$image->id."'>".$image->name."</option>";
+//            /*echo "<img src='".$image->source."'>";*/
+//        }
+//        $output .= '</select>';
+//        echo $output;
+//    }
+//    echo "probleme";
+//}else{
+//    echo 'probleme';
+//}
