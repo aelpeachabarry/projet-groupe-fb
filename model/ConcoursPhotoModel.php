fb03b60ad4d5f7f6ced1f69670e689b0baa04bc2
mrakotomizao <mrakotomizao@gmail.com>
2015-07-02 00:03:21 +0200

    New changelist

--- model/ConcoursPhotoModel.php
+++ model/ConcoursPhotoModel.php
@@ -5,8 +5,6 @@
  * Date: 01/07/2015
  * Time: 23:04
  */
-require 'core/Model.php';
-
 class ConcoursPhotoModel extends abstractModel{
     public function __construct(){
         parent::__construct('user_concours_photo');
