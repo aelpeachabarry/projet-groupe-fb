136e0bb8bd78e262542ab16ba6e43b409e8bded9
mrakotomizao <mrakotomizao@gmail.com>
2015-07-01 23:47:17 +0200

    New changelist

--- controller/ControllerGallery.php
+++ controller/ControllerGallery.php
@@ -1,7 +1,7 @@
 <?php
 
-require 'model/PhotoModel.php';
-require 'model/ConcoursPhotoModel.php';
+require  $_SERVER['DOCUMENT_ROOT'].'model/PhotoModel.php';
+require  $_SERVER['DOCUMENT_ROOT'].'model/ConcoursPhotoModel.php';
 
 class ControllerGallery{
     public function __construct(){
