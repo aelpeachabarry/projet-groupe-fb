a99a20f9c89147a31d8e7bc915731c588a9a549c
mrakotomizao <mrakotomizao@gmail.com>
2015-07-02 10:19:10 +0200

    New changelist

--- model/ConcoursPhotoModel.php
+++ model/ConcoursPhotoModel.php
@@ -5,7 +5,6 @@
  * Date: 01/07/2015
  * Time: 23:04
  */
-include_once dirname(__FILE__).'core/Model.php';
 
 class ConcoursPhotoModel extends abstractModel{
     public function __construct(){
--- model/PhotoModel.php
+++ model/PhotoModel.php
@@ -5,7 +5,6 @@
  * Date: 27/06/15
  * Time: 23:04
  */
-include_once dirname(__FILE__).'/core/Model.php';
 
 class PhotoModel extends abstractModel{
     private $tableName = "photos";
