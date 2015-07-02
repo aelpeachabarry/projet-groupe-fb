f80ddf21ab4bb1e75e2d73a45b42486903117d2a
mrakotomizao <mrakotomizao@gmail.com>
2015-07-03 00:53:41 +0200

    New changelist

--- controller/ControllerGallery.php
+++ controller/ControllerGallery.php
@@ -21,6 +21,7 @@ class ControllerGallery{
             'last_update' => date('Y-m-d G:i:s'),
         ];
         $imgManager->create($nonescape,$escape);
+
         $imageConcours= $userConcPhoto->read('*',['id_facebook'=>$idUser,'id_concours'=>4]);
 
         if($imageConcours){
