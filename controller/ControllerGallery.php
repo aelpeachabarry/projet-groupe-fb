43a0389b304d863670560908294020f88a01b686
mrakotomizao <mrakotomizao@gmail.com>
2015-07-03 01:17:10 +0200

    New changelist

--- controller/ControllerGallery.php
+++ controller/ControllerGallery.php
@@ -22,7 +22,7 @@ class ControllerGallery{
         $imgManager->create($nonescape,$escape);
 
         $imageConcours= $userConcPhoto->read('*',['id_facebook'=>$idUser,'id_concours'=>4]);
-
+        var_dump($imageConcours);
         if($imageConcours){
             echo "Trying update";
             $userConcPhoto->update(['id_photo'=>$idPhoto],['id_facebook'=>$idUser,'id_concours'=>4]);
