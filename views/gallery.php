204c9181cc266fb551495c7a40799f2431022801
Newwer21 <j-jac_ly@hotmail.fr>
2015-07-18 23:41:42 +0200

    button share la photo depuis la galerie new

--- views/gallery.php
+++ views/gallery.php
@@ -6,7 +6,15 @@ include 'controller/ControllerGallery.php';
 ?>
 
 <div id="fb-root"></div>
-<script>(function(d, s, id) {
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '384491318402733',
+            xfbml      : true,
+            version    : 'v2.0'
+        });
+    };
+    (function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
