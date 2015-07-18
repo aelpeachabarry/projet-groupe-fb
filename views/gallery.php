c9984c897ed6a0095021f9d1e68fc9297059f166
Newwer21 <j-jac_ly@hotmail.fr>
2015-07-18 19:55:18 +0200

    button share la photo depuis la galerie revenu à l'époque

--- views/gallery.php
+++ views/gallery.php
@@ -13,6 +13,29 @@ include 'controller/ControllerGallery.php';
         js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.4&appId=384491318402733";
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));</script>
+<script>
+    function share_prompt()
+    {
+        FB.ui(
+            {
+                method: 'feed',
+                name: 'Facebook Dialogs',
+                link: 'http://developers.facebook.com/docs/reference/dialogs/',
+                picture: 'http://fbrell.com/f8.jpg',
+                caption: 'Reference Documentation',
+                description: 'Dialogs provide a simple, consistent interface for applications to interface with users.',
+                message: 'Facebook Dialogs are easy!'
+            },
+            function(response) {
+                if (response && response.post_id) {
+                    alert('Post was published.');
+                } else {
+                    alert('Post was not published.');
+                }
+            }
+        );
+    }
+</script>
 <div class="row col-lg-offset-2 col-lg-8">
 
     <!--caroussel-->
@@ -36,6 +59,7 @@ include 'controller/ControllerGallery.php';
                         <p><a href="#" class="label label-danger photo-infos" data-image-id="" data-toggle="modal" data-title="This is my title" data-caption="Some lovely red flowers" data-image="http://lorempixel.com/400/300" data-target="#image-gallery">Zoom</a>
                             <a href="" class="label label-default">Like</a>
                         <div class="fb-like" data-href="<?php echo $imageUrl ?>" data-layout="standard" data-action="like" data-show-faces="false" data-share="true"></div>
+                        <input type="button" onclick="share_prompt()" value="Share" />
                         <!--<a onClick="window.open('http://www.facebook.com/sharer.php?s=100&amp;p[title]=<?php echo $title;?>&amp;p[summary]=<?php echo $summary;?>&amp;p[url]=<?php echo $url; ?>&amp;p[images][0]=<?php echo $image;?>','sharer','toolbar=0,status=0,width=548,height=325');" href="javascript: void(0)">Insert text or an image here.</a>-->
                         </p>
                     </div>
