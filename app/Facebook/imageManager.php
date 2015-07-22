cc6f5e626bb90447a30f3f4e6972ed61a01a490f
mrakotomizao <mrakotomizao@gmail.com>
2015-07-22 23:23:00 +0200

    test d'un object image

--- app/Facebook/imageManager.php
+++ app/Facebook/imageManager.php
@@ -54,8 +54,8 @@ class ImageManager{
             [
                 'object' => json_encode([
                     'title' => 'test',
-                    'image' => 'https://scontent.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/11666296_130736713926319_6506740644690119820_n.jpg?oh=6da5536a069bd098b84bb210b14c3ec3&oe=560EF713',
-                    'url' => 'https://scontent.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/11666296_130736713926319_6506740644690119820_n.jpg?oh=6da5536a069bd098b84bb210b14c3ec3&oe=560EF713',
+                    'image' => 'https://scontent.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/10985051_1608904306017786_8580743035900434070_n.jpg?oh=9b798061ef0c77814befd955708ba4b3&oe=564E7018',
+                    'url' => 'https://scontent.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/10985051_1608904306017786_8580743035900434070_n.jpg?oh=9b798061ef0c77814befd955708ba4b3&oe=564E7018',
                     'description' => 'mon super test',
                     'site_name' => 'travel_info'
                 ])
