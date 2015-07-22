0a0a074cddc7c96fa541361e1bb5f5c869baba05
Barry Aelpeacha <aelpeacha.barry@gmail.com>
2015-07-22 16:47:57 +0200

    correction du ROOT

--- model/ConcoursPhotoModel.php
+++ model/ConcoursPhotoModel.php
@@ -6,8 +6,8 @@
  * Time: 23:04
  */
 
-require $_SERVER['DOCUMENT_ROOT'].'core/Model.php';
-//require './core/Model.php';
+require ROOT.'core/Model.php';
+
 
 
 class ConcoursPhotoModel extends abstractModel{
