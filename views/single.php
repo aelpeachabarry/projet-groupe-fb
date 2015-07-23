5549792b92c9da7c5a74c2c7d2fa79b3a01fde19
Barry Aelpeacha <aelpeacha.barry@gmail.com>
2015-07-23 15:05:43 +0200

    modification getallimages

--- views/single.php
+++ views/single.php
@@ -10,7 +10,7 @@ $imageUrl = $gc->getImage($_GET['id']);
     <div id="image-single" class="col-lg-12">
         <div class="col-lg-offset-2 col-lg-8 col-md-offset-2 col-md-8 col-xs-6 thumb" data-groups='["wall"]'>
             <div class="thumbnail">
-                <img class="lazy img-responsive" data-original="<?php echo $imageUrl ?>" src="<?php echo $imageUrl ?>" alt="">
+                <img class="lazy img-responsive" data-original="<?php echo $imageUrl['url'] ?>" src="<?php echo $imageUrl['url'] ?>" alt="">
             </div>
         </div>
     </div>
