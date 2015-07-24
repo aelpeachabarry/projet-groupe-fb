9f986ce6d27fe67b2b841c9fa6b47d442ce51aa1
mrakotomizao <mrakotomizao@gmail.com>
2015-07-25 00:29:43 +0200

    correction de probleme d'upload

--- views/gallery.php
+++ views/gallery.php
@@ -25,7 +25,7 @@ include 'controller/ControllerGallery.php';
                     <div class="caption">
                         <h4>Thumbnail Headline</h4>
                         <p>short thumbnail description</p>
-                        <p><a href="<?php echo BASE_URL;?>/photo/<?php echo $image['id_photo']; ?>" class="label label-danger photo-infos" >Zoom</a>
+                        <p><a href="/photo/<?php echo $image['id_photo']; ?>" class="label label-danger photo-infos" >Zoom</a>
 
                             <!--<a onClick="window.open('http://www.facebook.com/sharer.php?s=100&amp;p[title]=<?php echo $title;?>&amp;p[summary]=<?php echo $summary;?>&amp;p[url]=<?php echo $url; ?>&amp;p[images][0]=<?php echo $image;?>','sharer','toolbar=0,status=0,width=548,height=325');" href="javascript: void(0)">Insert text or an image here.</a>-->
                         </p>
