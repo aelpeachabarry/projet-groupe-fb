9194e9ea2b90d051968f34166da535d652df06a9
mrakotomizao <mrakotomizao@gmail.com>
2015-07-24 03:06:04 +0200

    correction de probleme d'upload

--- app/Facebook/constants.php
+++ app/Facebook/constants.php
@@ -6,4 +6,6 @@
  * Time: 08:16
  */
 
-
+define('APP_ID','384491318402733');
+define('APP_SECRET','dab4c606e28695176a9d99dcc2a813c8');
+define('REDIRECT_URL','https://projet-groupe-fb.herokuapp.com/');
