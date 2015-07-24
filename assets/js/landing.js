c5ef2bde0d518b3b533e52cb5ba73a04df24a738
mrakotomizao <mrakotomizao@gmail.com>
2015-07-25 00:12:11 +0200

    correction de probleme d'upload

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
             url : '/functions/connectFb.php', // La ressource ciblée
             type:'get',
             success : function(url){
-                document.location.href=url;
+                document.location.href='/'+url;
             }
         });
     });
